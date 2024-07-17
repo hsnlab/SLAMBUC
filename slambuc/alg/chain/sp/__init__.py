df30ebe6a240b4fdd0cef1f43aa2031cf1de7046
Janos Czentye <czentye@tmit.bme.hu>
2024-07-17 16:07:35 +0200

    Add chain partitioning based on hop-limited dijkstra

--- slambuc/alg/chain/sp/__init__.py
+++ slambuc/alg/chain/sp/__init__.py
@@ -11,6 +11,4 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from .dp import *
-from .ser import *
-from .sp import *
+from .min import sp_chain_partitioning
